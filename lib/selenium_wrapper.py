f4cdea170efde59eb49c22c0fe513c636ed25859
WildSiphon <wildsiphon@protonmail.com>
2022-05-21 03:09:38 +0200

    Update webdrivers to support latest version

--- lib/selenium_wrapper.py
+++ lib/selenium_wrapper.py
@@ -2,37 +2,53 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from sys import platform
+from pathlib import Path
 
+ERROR_MSG_BASE = (
+    "Scrapping only the name and the number of contributions "
+    "from Google Maps public profile"
+)
 
 class SeleniumWrapper:
+        
     def __init__(self, browser):
-        self.browser = browser
+        # TODO check browser validity
+        self.browser = browser[0].upper() + browser[1:]
+        
         self.driver = None
-        DRIVER_PATH = ""
+        self._set_driver()
+        
 
-        # Detect OS
+    def _get_driver_extension(self):
         if platform.startswith("linux"):
-            EXT = ""
+            return ""
         elif platform.startswith("win32"):
-            EXT = ".exe"
-        else:
-            raise Exception(
-                "The use of selenium is not supported for this OS. "
-                'Only "linux" and "win32" are possible\n',
-                "Scrapping only the name and the number of contributions "
-                "from Google Maps public profile",
-                sep="\n",
-            )
+            return ".exe"
+        raise Exception(
+            "Mailfogle with selenium is not supported for this OS. "
+            'Only "linux" and "windows" are possible\n' + ERROR_MSG_BASE
+        )
 
-        # Choose the good driver
-        if self.browser == "chrome":
+    def _set_driver(self):
+        msg = (
+            "Your {browser} profile cannot be loaded. "
+            "It may be missing or inaccessible.\n" + ERROR_MSG_BASE
+        )
+
+        extension = self._get_driver_extension()
+
+        if self.browser == "Chrome":
             options = ChromeOptions()
-            DRIVER_PATH = f"./drivers/chromedriver{EXT}"
-            self.driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
-
-        elif self.browser == "firefox":
+            driver_filename = "chromedriver" + extension
+        elif self.browser == "Firefox":
             options = FirefoxOptions()
-            DRIVER_PATH = f"./drivers/geckodriver{EXT}"
-            self.driver = webdriver.Firefox(
-                options=options, executable_path=DRIVER_PATH
-            )
+            driver_filename = "geckodriver" + extension
+
+        driver_path = f"./drivers/{driver_filename}"
+        if not Path(driver_path).is_file():
+            raise Exception(msg.format(browser=self.browser))
+
+
+        self.driver = getattr(webdriver, self.browser)(
+            options=options, executable_path=driver_path
+        )
